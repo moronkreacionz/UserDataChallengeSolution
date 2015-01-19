7ee98a23639758164284fc429108b6b6b0581d71
moronkreacionz <moronkreacionz@gmail.com>
2015-01-19 18:33:46 +0530

    Initial Commit

---
+++ UserDataChallengeSolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserDataChallengeSolution
+//
+//  Created by Vijay Mehta on 1/19/15.
+//  Copyright (c) 2015 Vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserDataChallengeSolution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserDataChallengeSolution
+//
+//  Created by Vijay Mehta on 1/19/15.
+//  Copyright (c) 2015 Vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
